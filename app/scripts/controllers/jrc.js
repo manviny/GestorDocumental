f298971a901de9266c63312372795bf795e379a9
manviny <manol@manol.home>
2015-05-27 08:32:45 +0200

    Revert "Revert 42bad10..aa1938f"
    This reverts commit d5b2b06638e49b8bd1f266132bf3585a05c6a47c.

--- app/scripts/controllers/jrc.js
+++ app/scripts/controllers/jrc.js
@@ -8,23 +8,91 @@
  * Controller of the angularjsAuthTutorialApp
  */
 angular.module('angularjsAuthTutorialApp')
-  .controller('JrcCtrl', function ($scope, dfapi) {
+  .controller('JrcCtrl', function ($scope, dfapi, DreamFactory) {
     
-    $scope.rowCollection = [
-        {orden: '1', grupo: 'Grupo_Panstar', empresa: 'PNM_Panamar', tipo: '01-Registro', anyo: '2015', registro: '150114', documento: 'PNM-Panamar_#007-Cucas_Planos-Puntos-Control.pdf'},
-        {orden: '1', grupo: 'Grupo_Panstar2', empresa: 'PNM_Panamar2', tipo: '01-Registro', anyo: '2015', registro: '150114', documento: 'PNM-Panamar_#007-inse-Puntos-Control.pdf'},
-        {orden: '1', grupo: 'Grupo_Panstar3', empresa: 'PNM_Panamar3', tipo: '01-Registro', anyo: '2015', registro: '150114', documento: 'PNM-Panamar_#007-salta-Puntos-Control.pdf'},
+    // $scope.rowCollection = [
+    //     {Nosotros: '1', Grupo: 'Grupo_Panstar', Empresa: 'PNM_Panamar', Tipo: '01-Registro', anyo: '2015', mes: '150114', documento: 'PNM-Panamar_#007-Cucas_Planos-Puntos-Control.pdf'},
+    //     {Nosotros: '1', Grupo: 'Grupo_Panstar2', Empresa: 'PNM_Panamar2', Tipo: '01-Registro', anyo: '2015', mes: '150114', documento: 'PNM-Panamar_#007-inse-Puntos-Control.pdf'},
+    //     {Nosotros: '1', çGrupo: 'Grupo_Panstar3', Empresa: 'PNM_Panamar3', Tipo: '01-Registro', anyo: '2015', mes: '150114', documento: 'PNM-Panamar_#007-salta-Puntos-Control.pdf'},
 
-    ];
+    // ];
 
 
-    $scope.tableTitles = tableTitles.documentos;
 
-    dfapi.getFileFromDB('Palomillas')
-    .then(function(response){
+
+
+             DreamFactory.api.S3.getFile({
+                container: 'jrcnaturalsystems',
+                file_path: '/Grupo_Panstar/PNM_Panamar/01-Registro/2015/150114/PNM-Panamar_#007-Palomillas_Registro-de-vigilancia-Insectos.pdf'
+             },
+             // Success function
+              function(result) { 
+                // console.debug("RESULTADO",result);
+                // window.open("data:application/pdf," + escape(result));
+                // window.open("data:application/pdf;base64, " + result);
+                // var file = new Blob([result], {type: 'application/pdf'});
+                // var fileURL = URL.createObjectURL(file);
+                // window.open(fileURL, '_self', '');
+                // window.open("data:application/pdf," + escape(result));
+              },
+             // Error function
+             function(reject) { console.debug("Reject",reject);  });
+
+
+      
+     $scope.configDB = function(name){ 
+
+      $scope.nombreDB = name;
+      $scope.rowCampos = [];
+
+      dfapi.getFileFromDB(name)
+      .then(function(response){
+
+          console.debug("datos BD",response);
+            
+            $scope.tableTitles = response.titles;
+            var arrayFiles = [];
+            _.forEach(response.content, function(file) {
+                arrayFiles = file.path.split('/');              // convierte el path en un array
+                // 1.- este objetos 
+                var este = {url:file.path};
+                response.titles.forEach(function(col, index){
+                    var temp = response.titles[index];
+                    este[temp] =  arrayFiles[index];    
+                })            
+                $scope.rowCollection.push(este)
+            }); 
+            console.debug("rowCollection",$scope.rowCollection);
+
+
+
+            //roles
+            $scope.roles = response.roles;
+
+
+            // numero de datos encontrados
+            $scope.contentLength = response.content.length;
+
+
+
+
+      })
+
+     }
+
+
+    dfapi.setBucket('jrcnaturalsystems');
+    $scope.configDB('Insecto');
+
+
+
+    // $scope.tableTitles = tableTitles.documentos;
+
+    // dfapi.getFileFromDB('Palomillas')
+    // .then(function(response){
     	
-        console.log(response);
-    })
+    //     console.log(response);
+    // })
 
 
   });
--- app/scripts/services/dfapi.js
+++ app/scripts/services/dfapi.js
@@ -133,17 +133,19 @@ angular.module('angularjsAuthTutorialApp')
 		 */
   		var getFileFromDB = function (name) {
 			var deferred = $q.defer();
-		     DreamFactory.api.S3.getFile({
-		     	container: selectedBucket,
-		     	file_path:  '/' + dbprefix + name + '.json'
-		     },
-		     // Success function
-		      function(result) { 
-		      	deferred.resolve(result);
-		      },
-		     // Error function
-		     function(reject) { deferred.reject('No se pudo'); });
- 			return deferred.promise
+  			$rootScope.$on('api:ready', function(event) {
+			     DreamFactory.api.S3.getFile({
+			     	container: selectedBucket,
+			     	file_path:  '/' + dbprefix + name + '.json'
+			     },
+			     // Success function
+			      function(result) { 
+			      	deferred.resolve(result);
+			      },
+			     // Error function
+			     function(reject) { deferred.reject('No se pudo'); });
+ 			});
+	 		return deferred.promise
 		}
 
 
@@ -171,14 +173,13 @@ angular.module('angularjsAuthTutorialApp')
     			var este = {};
     			names.forEach(function(col, index){
              		var temp = names[index];
-              		este[temp] =  arrayFiles[index];	
+              		este[temp] = '"'+ arrayFiles[index]+'"';	
     			}) 				
 				$rootScope.rowCollection.push(este)
 			});	
 
   			console.debug("$rootScope.rowCollection",$rootScope.rowCollection);
 		}
-
 
 
 		/**
@@ -350,3 +351,45 @@ angular.module('angularjsAuthTutorialApp')
     };  
 
   });
+
+// jQuery needed, uses Bootstrap classes, adjust the path of templateUrl
+angular.module('angularjsAuthTutorialApp')
+.directive('pdfDownload', function() {
+    return {
+        restrict: 'E',
+        // templateUrl: '/path/to/pdfDownload.tpl.html',
+        template: '<a href="" class="btn btn-primary" ng-click="downloadPdf()">Download</a>',
+        scope: true,
+        link: function(scope, element, attr) {
+            var anchor = element.children()[0];
+ 
+            // When the download starts, disable the link
+            scope.$on('download-start', function() {
+                $(anchor).attr('disabled', 'disabled');
+            });
+ 
+            // When the download finishes, attach the data to the link. Enable the link and change its appearance.
+            scope.$on('downloaded', function(event, data) {
+                $(anchor).attr({
+                    href: 'data:application/pdf;base64,' + data,
+                    download: attr.filename
+                })
+                    .removeAttr('disabled')
+                    .text('Save')
+                    .removeClass('btn-primary')
+                    .addClass('btn-success');
+ 
+                // Also overwrite the download pdf function to do nothing.
+                scope.downloadPdf = function() {
+                };
+            });
+        },
+        controller: ['$scope', '$attrs', '$http', function($scope, $attrs, $http) {
+            $scope.downloadPdf = function() {
+                $scope.$emit('download-start');
+                $http.get($attrs.url).then(function(response) {
+                    $scope.$emit('downloaded', response.data);
+                });
+            };
+        }] 
+}});
